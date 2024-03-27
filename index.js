75c11be9987a957997cfd3badabf70d862d66bd5
Victor kamau <Victor Wangari>
2024-03-27 11:56:00 +0300

    initial commit

--- index.js
+++ index.js
@@ -27,22 +27,77 @@ const nestedMuppet = {
 
 // Strings
 
+
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const { moo, neigh, baa, oink, cluck } = {
+  moo: "cow",
+  neigh: "horse",
+  baa: "sheep",
+  oink: "pig",
+  cluck: "chicken"
 
+}
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const {bessie, dolly, babe, little} = {
+  bessie: "cow",
+  dolly: "sheep",
+  babe: "pig",
+  little: "chicken"
 
+}
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
+const {blackAndWhite, black, pink} = {
+  blackAndWhite: "cow",
+  black: "sheep",
+  pink: "pig"
+}
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const [red, orange, yellow, green, blue, indigo, violet]= [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "violet"
+
+]
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+const [r, o, y, g, b, i, v]= [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "violet"
 
+]
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const [indg] = [
+  "indigo"
+]
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+ const {muppetName, color, song, job, partner} = {
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit"
+ }
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const {song2, song4, nestedJob, nestedPartner} = {
+  song2: "Moving Right Along",
+  song4: "I Hope That Something Better Comes Along",
+  nestedJob: "Host of The Muppet Show",
+  nestedPartner: "Miss Piggy"
+}
+
